ff3f1e61aed65781edf98fbe7e959a95dc646936
SoboladSports <sobolevski@sports.ru>
2018-05-09 00:56:54 +0400

    searching and filtering functionality added

--- gp/TestCase/admin.py
+++ gp/TestCase/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from .models import Action, Condition, Element, Screen, Tag, TestCase, TestData, TestStep
 
 
-
+class TestCaseAdmin(admin.ModelAdmin):
+	exclude = ('slug',)
+	list_display = ('title', 'priority', 'created', 'edited', )
+	list_filter = ('priority', )
+	search_fields = ('title', 'description')
+		
 admin.site.register(Action)
 admin.site.register(Condition)
 admin.site.register(Element)
 admin.site.register(Screen)
 admin.site.register(Tag)
 admin.site.register(TestStep)
-admin.site.register(TestCase)
 admin.site.register(TestData)
+admin.site.register(TestCase, TestCaseAdmin)
 
