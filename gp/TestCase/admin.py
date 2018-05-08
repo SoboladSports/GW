d2b02e8d362c3eb343042c00bf644d6b67031b67
SoboladSports <sobolevski@sports.ru>
2018-05-09 00:18:10 +0400

    adding to admin backoffice some models

--- gp/TestCase/admin.py
+++ gp/TestCase/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Action, Condition, Element, Screen, Tag, TestCase, TestData, TestStep
 
-# Register your models here.
+
+
+admin.site.register(Action)
+admin.site.register(Condition)
+admin.site.register(Element)
+admin.site.register(Screen)
+admin.site.register(Tag)
+admin.site.register(TestStep)
+admin.site.register(TestCase)
+admin.site.register(TestData)
+
