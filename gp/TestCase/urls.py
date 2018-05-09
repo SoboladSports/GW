9c17930263fdec8b5773a19d4115551ba2184fef
SoboladSports <sobolevski@sports.ru>
2018-05-10 00:56:51 +0400

    list and detailed views are now added

--- gp/TestCase/urls.py
+++ gp/TestCase/urls.py
@@ -4,9 +4,9 @@ from .views import TestCaseListView, TestCaseDetailView, test_case_detail, test_
 app_name = 'TestCase'
 
 urlpatterns = [
-	url(r'^test-case-list/$', TestCaseListView.as_view(), name = 'test_case_list_view'),
-	url(r'^(?P<slug>[-\w]+)$', TestCaseDetailView.as_view(), name = 'test_case_detail_view'),
-	url(r'^test-case-list-function/$', test_case_list, name = 'test_case_list'),
-	url(r'^(?P<slug>[-\w]+)$', test_case_detail, name = 'test_case_detail'),
+	#url(r'^test-case-list/$', TestCaseListView.as_view(), name = 'test_case_list_view'),
+	#url(r'^test-case-detail/(?P<slug>[-\w]+)/$', TestCaseDetailView.as_view(), name = 'test_case_detail_view'),
+	url(r'^test-case-list/$', test_case_list, name = 'test_case_list'),
+	url(r'^test-case-detail/(?P<slug>[-\w]+)/$', test_case_detail, name = 'test_case_detail'),
 
 ]
