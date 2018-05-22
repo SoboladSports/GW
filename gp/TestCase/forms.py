bcefd56126e669ce739b41ec91c02cfc5b53e0b2
sobolevski <sobolevski@sports.ru>
2018-05-22 20:35:22 +0300

    trash

--- gp/TestCase/forms.py
+++ gp/TestCase/forms.py
@@ -1,7 +1,42 @@
 from django import forms
+from django.forms.models import inlineformset_factory
+from django.forms.models import BaseInlineFormSet
 
-from .models import Action, Condition, Element, Screen, Tag, TestCase, TestData, TestStep, Project
+from .models import Project, Screen, Element, TestData, Action, Condition, Step, TestStep, Tag, TestCase, Cases, TestCycle
 
+'''class BaseTestCaseFormset(BaseInlineFormSet):
+
+    def add_fields(self, form, index):
+        super(BaseTestCaseFormset, self).add_fields(form, index)
+
+
+        try:
+            instance = self.get_queryset()[index]
+            pk_value = instance.pk
+        except IndexError:
+            instance = None
+            pk_value = hash(form.prefix)
+
+        form.nested = [
+
+        ]
+
+
+TestCaseFormset = inlineformset_factory(TestCase, Condition, formset=BaseTestCaseFormset, extra=1)
+'''
+
+
+
+'''
+class ConditionForm(forms.ModelForm):
+    class Meta:
+        model = Condition
+        fields = ['title', 'screen', 'element', 'action', 'testdata', 'project',]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'screen': forms.Select(attrs={'class': 'form-control'}),
+            'element': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 class TestCaseForm(forms.ModelForm):
     class Meta:
@@ -31,3 +66,4 @@ class TestCaseView(forms.ModelForm):
             'teststep': forms.CheckboxSelectMultiple(attrs={'class': 'form-control', 'readonly': True}),
             'tag': forms.CheckboxSelectMultiple(attrs={'class': 'form-control', 'readonly': True}),
         }
+'''
